763d1d1f73b6b8cbf6f79837c578f20235291724
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-23 10:55:46 +0100

    test reset

--- contact.php
+++ contact.php
@@ -1,3 +1,14 @@
 <?php
 // contact trop super github
  ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>test github</title>
+   </head>
+   <body>
+     <h1>Guy Teube</h1>
+   </body>
+ </html>
